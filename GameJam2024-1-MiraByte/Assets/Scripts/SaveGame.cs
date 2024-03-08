978e888efe07d8fb054d64c7fc3806bb54d78903
bakosrozsa <bakosrozsa01@gmail.com>
2024-03-08 17:25:10 +0100

    Load data in xml

--- GameJam2024-1-MiraByte/Assets/Scripts/SaveGame.cs
+++ GameJam2024-1-MiraByte/Assets/Scripts/SaveGame.cs
@@ -11,7 +11,7 @@ public class SaveGame : MonoBehaviour
 
     void Start()
     {
-        //SaveXml();
+        //LoadXml();
     }
     private void SaveXml() 
     {
@@ -27,15 +27,49 @@ public class SaveGame : MonoBehaviour
         playerPosY.InnerText = player.transform.position.y.ToString();
         root.AppendChild(playerPosY);
 
-        XmlElement prisonerOrGuard = xmlDoc.CreateElement("prisonerOrGuard");
+        XmlElement prisonerOrGuard = xmlDoc.CreateElement("PrisonerOrGuard");
         prisonerOrGuard.InnerText = player.GetComponent<PlayerBehaviour>().playerType.ToString();
         root.AppendChild(prisonerOrGuard);
 
         xmlDoc.AppendChild(root);
         xmlDoc.Save(Application.dataPath + "/Data.text");
-        if (File.Exists(Application.dataPath + "Data.text"))
+        if (File.Exists(Application.dataPath + "/Data.text"))
         {
             Debug.Log("Saved");
+        }
+    }
+    private void LoadXml() 
+    {
+        if (File.Exists(Application.dataPath + "/Data.text"))
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Application.dataPath + "/Data.text");
+
+            XmlNodeList playerPosXXml = xmlDoc.GetElementsByTagName("PlayerPosX");
+            float playerPosX = float.Parse(playerPosXXml[0].InnerText);
+            
+
+            XmlNodeList playerPosYXml = xmlDoc.GetElementsByTagName("PlayerPosY");
+            float playerPosY = float.Parse(playerPosYXml[0].InnerText);
+
+            XmlNodeList prisonerOrGuard = xmlDoc.GetElementsByTagName("PrisonerOrGuard");
+            PlayerType playerType;
+            if (prisonerOrGuard[0].InnerText == "Prisoner")
+            {
+                playerType = PlayerType.Prisoner;
+            }
+            else
+            {
+                playerType = PlayerType.Prisoner;
+            }
+            
+            player.transform.position = new Vector2(playerPosX,playerPosY);
+            player.GetComponent<PlayerBehaviour>().playerType = playerType;
+
+        }
+        else 
+        {
+            Debug.Log("file doesn't exist");
         }
     }
 }
