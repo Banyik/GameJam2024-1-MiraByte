aef2dbc8ab3e980485db861bd0c53eb55bdd116b
bakosrozsa <bakosrozsa01@gmail.com>
2024-03-11 23:54:17 +0100

    muzsika szabályozás

--- GameJam2024-1-MiraByte/Assets/Scripts/PlayVolume.cs
+++ GameJam2024-1-MiraByte/Assets/Scripts/PlayVolume.cs
@@ -2,14 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Volume : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
     public GameObject soundWave;
+    public Slider slider;
+
+    public void Start()
+    {
+        if (!Volumevalue.isCalled)
+        {
+            Volumevalue.isCalled = true;
+        }
+        else
+        {
+            slider.value = Volumevalue.volume;
+        }
+        
+    }
 
     public void SetVolume(float sliderValue) 
     {
+        Volumevalue.volume = sliderValue;
         audioMixer.SetFloat("mastervol",Mathf.Log10(sliderValue) * 20);
         if (sliderValue > 0.001)
         {
