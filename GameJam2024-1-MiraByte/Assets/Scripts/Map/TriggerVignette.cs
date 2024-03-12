aa4ee69e147f841399836c8d457ab4e3bf043ef0
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2024-03-12 15:15:01 +0100

    Added nightvision function in vents

--- GameJam2024-1-MiraByte/Assets/Scripts/Map/TriggerVignette.cs
+++ GameJam2024-1-MiraByte/Assets/Scripts/Map/TriggerVignette.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using Player;
+using System.Linq;
 
 public class TriggerVignette : MonoBehaviour
 {
-    public float vignetteStrength;
+    public float vignetteStrengthWithNightVision;
+    public float vignetteStrengthWithoutNightVision;
     public VolumeProfile volume;
 
+    public Light2D globalLight;
+    public Color nightVision;
+    public Color normalVision;
     Vignette vignette;
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -16,7 +22,16 @@ public class TriggerVignette : MonoBehaviour
         if(collision.tag == "Player")
         {
             volume.TryGet(out vignette);
-            vignette.intensity.value = vignetteStrength;
+            if(collision.GetComponent<Inventory>().items.Where(x => x.item == ItemTypes.NightVisionGoogles).ToList().Count > 0)
+            {
+                vignette.intensity.value = vignetteStrengthWithNightVision;
+                globalLight.color = nightVision;
+            }
+            else
+            {
+                vignette.intensity.value = vignetteStrengthWithoutNightVision;
+                globalLight.color = normalVision;
+            }
         }
     }
 }
