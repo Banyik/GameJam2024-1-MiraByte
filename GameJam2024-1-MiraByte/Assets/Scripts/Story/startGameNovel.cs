4772665c40f9f6551146023b849fcb65e748c751
bakosrozsa <bakosrozsa01@gmail.com>
2024-03-12 15:39:37 +0100

    Add start novel without text

---
+++ GameJam2024-1-MiraByte/Assets/Scripts/Story/startGameNovel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startGameNovel : MonoBehaviour
+{
+    public List<GameObject> pages;
+    public int pageCount;
+    public void Start()
+    {
+        pages[0].SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && pages.Count - 1 == pageCount)
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (Input.GetMouseButtonDown(0) && pages.Count - 1 > pageCount)
+        {
+            pageCount++;
+            pages[pageCount].SetActive(true);
+            pages[pageCount - 1].SetActive(false);
+        }
+    }
+}
